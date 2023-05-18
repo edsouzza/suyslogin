69789aa235447a8e1592075f6c6e6adda0b54d1a
Edi Aquino de Souza <edsouzza@hotmail.com>
2023-05-18 10:49:33 -0300

    update main

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Syslogin</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Titulo Principal</h1>
+    <h1>Ola mundo</h1>
 </body>
 </html>
