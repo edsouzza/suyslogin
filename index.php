7b8a2a77b9cc08974fb4b295d30ef47f099f3c05
Edi Aquino de Souza <edsouzza@hotmail.com>
2023-05-18 09:49:08 -0300

    configurando projeto inicial

--- index.php
+++ index.php
@@ -4,10 +4,68 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- GOOGLE FONTS LATO -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet">
+
+    <!-- BOOTSTRAP ICONS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    <!-- CSS BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <!-- CSS DO PROJETO -->
     <link rel="stylesheet" href="css/style.css">
+
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"
+        defer>
+    </script>
+
     <title>Syslogin</title>
 </head>
 <body>
-    <h1>Titulo Principal</h1>
+    <div id="header">
+        <nav class="navbar navbar-expand-lg fixed-top " id="navbar">
+            <div class="container py-3">
+                <!-- CONTAINER -->
+                <a href="#" class="navbar-brand primary-color">
+                    <img src="img/logo.png" alt="Syspdv">
+                </a>
+                <!-- MENU MOBILE -->
+                <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbar-items"
+                    aria-controls="navbar-items" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="bi bi-list"></i>
+                </button><!-- /MENU MOBILE -->
+
+                <div class="collapse navbar-collapse" id="navbar-items">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item ">
+                            <a href="#home" class="nav-link active" aria-current="page">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#empresa" class="nav-link">Quem somos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#videos" class="nav-link">Vídeos Funcionalidades</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#licenca" class="nav-link">Licença</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="assets/politicas.pdf" target="_blank" rel="noopener noreferrer" class="nav-link">Política</a>
+                        </li>
+                    </ul>
+                </div>
+            </div><!-- CONTAINER -->
+        </nav><!-- NAVBAR -->
+    </div><!-- HEADER -->
+
 </body>
 </html>
