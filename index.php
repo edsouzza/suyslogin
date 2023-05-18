a67ae2f163d8dbd8ee470893e1c784c3a000cb28
Edi Aquino de Souza <edsouzza@hotmail.com>
2023-05-18 10:02:52 -0300

    update menu

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     
     <!-- GOOGLE FONTS LATO -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -21,7 +20,7 @@
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
     <!-- CSS DO PROJETO -->
-    
+    <link rel="stylesheet" href="css/style.css">
 
     <!-- BOOTSTRAP JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
